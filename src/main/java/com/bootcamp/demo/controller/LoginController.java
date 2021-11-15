7f0ae87a46e155dd8d696051a773a444684f60bb
Kapaceala <eduard.caraenacherot@gmail.com>
2021-11-15 14:06:32 +0200

    Șters import inutil.

--- src/main/java/com/bootcamp/demo/controller/LoginController.java
+++ src/main/java/com/bootcamp/demo/controller/LoginController.java
@@ -5,7 +5,6 @@ import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
