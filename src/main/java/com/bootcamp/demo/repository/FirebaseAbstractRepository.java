f423be6221eedc19fd14cee686d78568f7d29f9e
Ungureanu Florin-Catalin <florin.g.ungureanu@gmail.com>
2021-11-19 01:52:55 +0200

    fully generic implementation of DAO

---
+++ src/main/java/com/bootcamp/demo/repository/FirebaseAbstractRepository.java
@@ -0,0 +1,90 @@
+package com.bootcamp.demo.repository;
+
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.*;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.ParameterizedType;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
+
+import com.google.cloud.firestore.CollectionReference;
+import org.apache.commons.beanutils.PropertyUtils;
+
+public abstract class FirebaseAbstractRepository<T> implements IRepository<T> {
+    private final Class<T> className;
+
+    @SuppressWarnings("unchecked")
+    public FirebaseAbstractRepository() {
+        this.className = ((Class<T>) (
+                (ParameterizedType) getClass().getGenericSuperclass()).getActualTypeArguments()[0]);
+    }
+
+    public abstract CollectionReference getCollection();
+
+
+    public Set<T> findAll() {
+        try {
+            Iterable<QueryDocumentSnapshot> documents = getCollection()
+                    .get()
+                    .get()
+                    .getDocuments();
+            Set<T> result = new HashSet<>();
+            for (QueryDocumentSnapshot doc : documents) {
+                T object = doc.toObject(className);
+                PropertyUtils.setProperty(object, "id", doc.getId());
+                result.add(object);
+            }
+            return result;
+        } catch (ExecutionException | InterruptedException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            throw new RuntimeException();
+        }
+    }
+
+
+    public String save(T t, String id) {
+        if (null == t) throw new IllegalArgumentException();
+        try {
+            ApiFuture<WriteResult> collectionFuture = getCollection()
+                    .document(id)
+                    .set(t);
+            return collectionFuture.get().getUpdateTime().toString();
+        } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException();
+        }
+    }
+
+
+    public String remove(String id) {
+        if (null == id) throw new IllegalArgumentException();
+        try {
+            ApiFuture<WriteResult> collectionFuture = getCollection()
+                    .document(id)
+                    .delete();
+            return collectionFuture.get().getUpdateTime().toString();
+        } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException();
+        }
+    }
+
+
+    public T findById(String id) {
+        DocumentReference documentReference = getCollection().document(id);
+        try {
+            ApiFuture<DocumentSnapshot> future = documentReference.get();
+            DocumentSnapshot documentSnapshot = future.get();
+
+            T t;
+            if (documentSnapshot.exists()) {
+                t = documentSnapshot.toObject(className);
+                return t;
+            } else {
+                return null;
+            }
+        } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException();
+        }
+    }
+
+}
