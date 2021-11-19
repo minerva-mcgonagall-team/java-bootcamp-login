5ff83777f191219c449b61a19508fa9d6ccd38f5
Kapaceala <eduard.caraenacherot@gmail.com>
2021-11-19 13:36:30 +0200

    #Refacere inainte de IntegrareLogin

--- src/main/java/com/bootcamp/demo/controller/ControllerLogin.java
+++ src/main/java/com/bootcamp/demo/controller/ControllerLogin.java
@@ -1,16 +1,11 @@
 package com.bootcamp.demo.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ControllerLogin {
 
-    private final LoginService loginService;
-    //am nevoie de ea pentru maparea GET
 
 
     @RequestMapping("/login")
@@ -30,10 +25,5 @@ public class ControllerLogin {
         return "login-error";
     }
     //Mapare pe eroare de login, adică nu a găsit niciun user:(
-
-    @GetMapping
-    public String loginService(@RequestBody String email, String password) {
-        return loginService.loginUser(email, password);
-    }//preia informatiile pentru logare
 
 }
