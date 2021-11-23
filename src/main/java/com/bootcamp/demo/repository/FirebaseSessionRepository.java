3fa61ffe48c787e0efa65f40bb247aa01bc1ca13
Ungureanu Florin-Catalin <florin.g.ungureanu@gmail.com>
2021-11-23 03:19:00 +0200

    getAllActiveSessions and update, theoretically should work

--- src/main/java/com/bootcamp/demo/repository/FirebaseSessionRepository.java
+++ src/main/java/com/bootcamp/demo/repository/FirebaseSessionRepository.java
@@ -2,16 +2,21 @@ package com.bootcamp.demo.repository;
 
 
 import com.bootcamp.demo.model.Session;
+import com.bootcamp.demo.model.User;
 import com.google.cloud.firestore.*;
 
 
+import java.util.HashSet;
+import java.util.Set;
+
+import java.util.concurrent.ExecutionException;
 
 
-
-public class FirebaseSessionRepository extends FirebaseAbstractRepository<Session> implements SessionRepository{
+public class FirebaseSessionRepository extends FirebaseAbstractRepository<Session> implements SessionRepository {
     Firestore firestoreDB;
 
     private static final String COLLECTION_PATH = "sessions";
+
     public FirebaseSessionRepository(Firestore firestoreDB) {
         this.firestoreDB = firestoreDB;
     }
@@ -20,6 +25,35 @@ public class FirebaseSessionRepository extends FirebaseAbstractRepository<Sessio
     @Override
     public CollectionReference getCollection() {
         return firestoreDB.collection(COLLECTION_PATH);
+    }
+
+    @Override
+    public Boolean updateSession(Session session) {
+        if (null == session) {
+            throw new IllegalArgumentException();
+        }
+        save(session, session.getId());
+        return true;
+    }
+
+    @Override
+    public Set<Session> getAllActiveSessions(User user) {
+        try {
+            Iterable<QueryDocumentSnapshot> documents = getCollection()
+                    .get()
+                    .get()
+                    .getDocuments();
+            Set<Session> result = new HashSet<>();
+            for (QueryDocumentSnapshot doc : documents) {
+                Session session = doc.toObject(Session.class);
+                if ((session.getUser().equals(user)) && (session.getEndSession() == null)) {
+                    result.add(session);
+                }
+            }
+            return result;
+        } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException();
+        }
     }
 }
 
