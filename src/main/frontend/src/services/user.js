7c15fbac5e0fdca72d93e72a56c79cb33caba7f3
Ungureanu Florin-Catalin <florin.g.ungureanu@gmail.com>
2021-12-07 19:08:41 +0200

    I just push and hope for the best

--- src/main/frontend/src/services/user.js
+++ src/main/frontend/src/services/user.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const API_URL = 'http://localhost:8080/test';
+const API_URL = 'https://java-bootcamp-login.herokuapp.com/test/';
 
 class User {
   getPublicContent() {
--- src/main/java/com/bootcamp/demo/dto/builder/UserBuilder.java
+++ src/main/java/com/bootcamp/demo/dto/builder/UserBuilder.java
@@ -4,13 +4,14 @@ import com.bootcamp.demo.dto.request.RegisterRequest;
 import com.bootcamp.demo.model.User;
 
 public class UserBuilder {
-    public UserBuilder(){
+    public UserBuilder() {
     }
-    public static User toEntity(RegisterRequest dto){
+
+    public static User toEntity(RegisterRequest dto) {
         return new User(dto.getFirstname(),
-                        dto.getLastname(),
-                        dto.getEmail(),
-                        dto.getPassword(),
-                        dto.getPhoneNumber());
+                dto.getLastname(),
+                dto.getEmail(),
+                dto.getPassword(),
+                dto.getPhoneNumber());
     }
 }
